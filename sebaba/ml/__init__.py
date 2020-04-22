e733d591d15b437955b3f97f3071f43ab9faa313
noelnamai <@RiverFront$2016>
2020-04-22 12:16:46 -0400

    update

--- sebaba/ml/__init__.py
+++ sebaba/ml/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from ml.NearestNeighbor import KNNClassifier
-from ml.NearestNeighbor import KNNRegression
-from ml.Clustering import KMeans
-from ml.Clustering import KMedians
-from ml.Perceptron import Perceptron 
-from ml.LinearRegression import LinearRegression
-from ml.LinearRegression import RidgeRegression
-from ml.LogisticRegression import LogisticClassifier
-from ml.LogisticRegression import RidgeClassifier
-from ml.SupportVectorMachine import SVMClassifier
-from ml.DimensionReduction import PCA
+from .nearest_neighbor import KNNClassifier
+from .nearest_neighbor import KNNRegression
+from .clustering import KMeans
+from .clustering import KMedians
+from .perceptron import Perceptron 
+from .linear_regression import LinearRegression
+from .linear_regression import RidgeRegression
+from .logistic_regression import LogisticClassifier
+from .logistic_regression import RidgeClassifier
+from .support_vector_machine import SVMClassifier
+from .dimension_reduction import PCA
