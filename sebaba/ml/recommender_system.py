5d0d9bfeb48e3ad89d4be86a2c8ea6c2abff90ee
noelnamai <@RiverFront$2016>
2020-04-26 14:45:28 -0400

    update

--- sebaba/ml/recommender_system.py
+++ sebaba/ml/recommender_system.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 
+from scipy.optimize import minimize
+
 
 class CollaborativeFiltering(object):
     """
@@ -9,53 +11,72 @@ class CollaborativeFiltering(object):
 
     Parameters
     --------------------------------------------------
+        alpha     : float the learning rate
+        gamma     : float the regularization parameter
         iterations: int maximum number of iterations to be performed
     """
-    def __init__(self, iterations = 100):
-        self.iters     = iterations
-        self.tolerance = 1.0e-16
+    def __init__(self, alpha = 0.01, gamma = 0.01, n_features = 10, iterations = 100):
+        self.alpha      = alpha
+        self.gamma      = gamma
+        self.iters      = iterations
+        self.n_features = n_features
+        self.tolerance  = 1.0e-16
 
     def fit(self, y, is_rated):
-        m = y.shape[0]
-        n = y.shape[1]
+        n_movies = y.shape[0]
+        n_users  = y.shape[1]
 
-        theta  = np.ones((n, 100))
-        x      = np.zeros((m, 100))        
+        x     = np.random.random(size = (n_movies, self.n_features))  
+        theta = np.random.random(size = (n_users, self.n_features))  
+
+        params = np.concatenate((x.flatten(), theta.flatten()))        
         y_norm = self.normalize_ratings(y, is_rated)
 
-        self.gradient_descent(x, y_norm, theta, is_rated)
+        opt_result = minimize(
+            fun = self.gradient, 
+            x0 = params,
+            args = (y_norm, is_rated),
+            method = "CG", 
+            jac = True,
+            tol = self.tolerance,
+            options = {"maxiter": self.iters}
+        )
 
-    def cost_function(self, y, y_prime, is_rated):
-        cost = (1 / 2) * np.sum(((y_prime - y) ** 2) * is_rated)
+        return opt_result
 
-        return cost
+    def gradient(self, params, y, is_rated):
+        n_movies = y.shape[0]
+        n_users  = y.shape[1]
+        
+        x     = np.reshape(params[ :n_movies * self.n_features], (n_movies, self.n_features))
+        theta = np.reshape(params[n_movies * self.n_features: ], (n_users, self.n_features))
 
-    def gradient_descent(self, x, y, theta, is_rated):
-        self.cost = list()
+        #np.dot(x, theta) = sum(theta.T * x)
+        y_prime = np.dot(x, theta.T)
 
-        prev_cost = np.inf
+        cost = (1 / 2) * np.sum(((y_prime - y) * is_rated) ** 2)
+        cost = cost + ((self.gamma / 2) * np.sum(x ** 2))
+        cost = cost + ((self.gamma / 2) * np.sum(theta ** 2))
 
-        for _ in range(int(self.iters)):
-            #np.dot(x, theta) = sum(theta.T * x)
-            y_prime   = np.dot(x, theta.T)
-            x         = x - np.dot(((y_prime - y) * is_rated), theta)
-            theta     = theta - np.dot(((y_prime - y) * is_rated).T, x)
-            curr_cost = self.cost_function(y, y_prime, is_rated)
-            self.cost.append(curr_cost)
+        x_grad     = self.alpha * ((np.dot(((y_prime - y) * is_rated), theta)) + self.gamma * x)
+        theta_grad = self.alpha * ((np.dot(((y_prime - y) * is_rated).T, x)) + self.gamma * theta)
 
-        self.x     = x
-        self.theta = theta
+        params = np.concatenate((x_grad.flatten(), theta_grad.flatten()))
+
+        return cost, params
 
     def normalize_ratings(self, y, is_rated):
-        m = y.shape[0]
-        n = y.shape[1]
+        n_movies = y.shape[0]
+        n_users  = y.shape[1]
 
-        y_mean = np.zeros((m, 1))
-        y_norm = np.zeros((m, n))
+        y_mean = np.zeros((n_movies, 1))
+        y_norm = np.zeros((n_movies, n_users))
 
-        for i in range(m):
-            idx            = is_rated[i,:] != 0
+        for i in range(n_movies):
+            idx            = is_rated[i, :] != 0
             y_mean[i]      = np.mean(y[i, idx])
             y_norm[i, idx] = y[i, idx] - y_mean[i]
+
+        self.y_mean = y_mean
 
         return y_norm
